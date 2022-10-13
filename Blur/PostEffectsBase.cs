c71bcadca00625196a63bf8f60a5e241add72272
wenruo <wankcn@icloud.com>
2022-10-13 18:51:38 +0800

    高斯模糊10.13

---
+++ Blur/PostEffectsBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent (typeof(Camera))]
+public class PostEffectsBase : MonoBehaviour {
+
+	public Shader shader;
+	public Material material;
+	protected bool effect_enable = false;
+	void Start()
+	{
+		Check();
+	}
+	protected void Check() {
+		if (!SystemInfo.supportsImageEffects) {
+			effect_enable = false;
+			return;
+		}
+
+		if (!shader || !shader.isSupported || 
+			!material || material.shader != shader) {
+			effect_enable = false;
+			return;
+		}
+
+		effect_enable = true;
+	}
+}
